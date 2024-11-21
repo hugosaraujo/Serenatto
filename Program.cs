7172a03224af72fd196906f829dae124af2c996c
hugo de araujo <hugo.saraujo@tutanota.com>
2024-11-21 15:45:50 -0300

    Testando os métodos de consultas linq com a lista formas de pagamento

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using SerenattoEnsaio.Dados;
+using SerenattoEnsaio.Modelos;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+IEnumerable<Cliente> clientes = DadosClientes.GetClientes().ToList();
+IEnumerable<string> formasPagamento = DadosFormaDePagamento.FormasDePagamento;
+
+Console.WriteLine("RELATÓRIO DE DADOS CLIENTES");
+foreach(var cliente in clientes)
+{
+    Console.WriteLine($"{cliente.Id} | {cliente.Nome} | {cliente.Endereco} | {cliente.Telefone}");
+}
+
+
+Console.WriteLine("-------------------------------------------------------------------------------");
+var pesquisa = from p in formasPagamento
+               where p.Contains('i')
+               select p;
+
+Console.WriteLine(string.Join(" ", pesquisa));
+
+var pesquisaLinq = formasPagamento.Where(p => p.StartsWith('p'));
+Console.WriteLine(string.Join(" ", pesquisaLinq));
